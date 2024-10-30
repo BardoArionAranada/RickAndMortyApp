5954641b7d49abd7d7615b3c1da4f8c91a04be26
arion aranda <arioncronos15s@gmail.com>
2024-10-29 23:37:04 -0600

    Configuración de MainActivity para iniciar con NavGraph

--- app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -3,27 +3,21 @@ package com.example.rickandmortyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.rickandmortyapp.ui.NavGraph
 import com.example.rickandmortyapp.ui.theme.RickAndMortyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RickAndMortyAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    AppContent()
                 }
             }
         }
@@ -31,17 +25,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppContent() {
+    val navController = rememberNavController()
+    NavGraph(navController = navController)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     RickAndMortyAppTheme {
-        Greeting("Android")
+        AppContent()
     }
 }
